58933c0c61f3a2d8cff94e1fd04214d1330094a6
mulika <mulika@nkbvs.ttn.ru>
2020-09-24 16:53:02 +0300

    applied clang-format

--- include/interface/publisher.h
+++ include/interface/publisher.h
@@ -2,13 +2,13 @@
 
 #include <string>
 
-namespace dds_test_app {
-class IPublisher {
-public:
-  
-  virtual bool init(int rate, const std::string &topic_name,
-                    const std::string &profile_name = "test-app-pub-profile") = 0;
-  virtual void run() = 0;
-  virtual ~IPublisher(){};
-};
+namespace dds_test_app
+{
+  class IPublisher
+  {
+  public:
+    virtual bool init(int rate, const std::string &topic_name, const std::string &profile_name = "test-app-pub-profile") = 0;
+    virtual void run()                                                                                                   = 0;
+    virtual ~IPublisher(){};
+  };
 } // namespace dds_test_app
--- include/interface/subscriber.h
+++ include/interface/subscriber.h
@@ -1,13 +1,13 @@
 #pragma once
 
-namespace dds_test_app {
-class ISubscriber {
-public:
-  virtual bool
-  init(const std::string &topic_name,
-       const std::string &profile_name = "test-app-sub-profile") = 0;
-  virtual void run() = 0;
-  virtual ~ISubscriber(){};
-};
+namespace dds_test_app
+{
+  class ISubscriber
+  {
+  public:
+    virtual bool init(const std::string &topic_name, const std::string &profile_name = "test-app-sub-profile") = 0;
+    virtual void run()                                                                                         = 0;
+    virtual ~ISubscriber(){};
+  };
 
 } // namespace dds_test_app
